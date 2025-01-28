423f4511f4bbd4391c4e4590905740b9b61dddc1
Hubert <sejnowskihubert5@gmail.com>
2025-01-28 21:02:15 +0000

    dodanie funkcji czydoskonala()

--- liczba.cpp
+++ liczba.cpp
@@ -2,9 +2,20 @@
 
 using namespace std;
 
+bool czy_doskonala(int liczba) {
+    if (liczba <= 1) return false;
+    int suma = 0;
+    for (int i = 1; i < liczba; ++i) {
+        if (liczba % i == 0) {
+            suma += i;
+        }
+    }
+    return suma == liczba;
+}
+
 int main() {
     int liczba;
-    cin >> liczba;  
-    cout << liczba <<endl;
+    cin >> liczba;
+    cout << liczba << (czy_doskonala(liczba) ? " jest doskonała" : " nie jest doskonała") << endl;
     return 0;
 }
