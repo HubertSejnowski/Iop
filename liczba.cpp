c35434332bbfca439965099ef30164e672ab35a9
Hubert <sejnowskihubert5@gmail.com>
2025-01-28 21:05:27 +0000

    dodanie sprawdzania czy liczba jest dodatnia

--- liczba.cpp
+++ liczba.cpp
@@ -1,10 +1,23 @@
- #include <iostream>
+#include <iostream>
 
-using namespace std;
+bool czy_doskonala(int liczba) {
+    if (liczba <= 1) return false;
+    int suma = 0;
+    for (int i = 1; i < liczba; ++i) {
+        if (liczba % i == 0) {
+            suma += i;
+        }
+    }
+    return suma == liczba;
+}
 
 int main() {
     int liczba;
-    cin >> liczba;  
-    cout << liczba <<endl;
+    std::cin >> liczba;
+    if (liczba <= 0) {
+        std::cout << "Liczba musi być dodatnią liczbą naturalną!" << std::endl;
+        return 1;
+    }
+    std::cout << liczba << (czy_doskonala(liczba) ? " jest doskonała" : " nie jest doskonała") << std::endl;
     return 0;
 }
